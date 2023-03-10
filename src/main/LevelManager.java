435f97aa98dcb9f53f4bbd9c1f733551577e557c
Gabriel Ferreira <gsf.ferreira99@gmail.com>
2023-03-10 17:09:13 -0400

    inserido sistema de fases

---
+++ src/main/LevelManager.java
@@ -0,0 +1,26 @@
+package main;
+
+import java.io.IOException;
+
+public class LevelManager {
+    public Level[] levels = new Level[6];
+    Game gp;
+    int levelActive = 1;
+    public LevelManager(Game gp){
+        this.gp = gp;
+        defineLevels();
+    }
+
+    public void defineLevels(){
+        levels[0] = new Level(this.gp, 1, "res/maps/fase_1.txt");
+        levels[1] = new Level(this.gp, 2, "res/maps/fase_2.txt");
+    }
+
+    public void setLevel(int level){
+        levelActive = level;
+    }
+
+    public String getLevelMap() {
+        return levels[levelActive-1].mapFile;
+    }
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,21 +1,29 @@
 package main;
 
 import javax.swing.JFrame;
+
 public class Main {
+    JFrame window;
 
     public static void main(String[] args) {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setResizable(false);
+        window.setResizable(true);
+        // Define o titulo da janela do jogo
         window.setTitle("JOGUINHO");
 
-        Game gamePanel = new Game();
+        // Instancia a classe de controle do jogo
+        Game gamePanel = new Game(window);
+        // Adiciona o gamePanel a janela Principal
         window.add(gamePanel);
+
         window.pack();
 
         window.setLocationRelativeTo(null);
         window.setVisible(true);
 
+        // Inicia o loop principal do jogo
         gamePanel.startHameThread();
     }
+
 }
